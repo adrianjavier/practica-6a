0d649fc1bcdb2a958af8babeb47ded6b85e81607
adrianjavier <57151458+adrianjavier@users.noreply.github.com>
2020-04-12 20:49:04 +0200

    corregido errores del script 6

--- script6.sh
+++ script6.sh
@@ -4,12 +4,12 @@ if [ $x -le 50 ]; then
 echo "Cuesta 50 euros"
 else if [ $x -le 250 ]; then
 	x=`expr $x - 50`
-	precio=`bc <<< echo "scale=2; $x \* 0.2"`
+	precio=`echo "scale=2 $x*0.2+20 | bc`
 	precio=`expr $precio + 50`
 	echo "El precio es de $precio euros"
 	else 
 	x=`expr $x - 250`
-	precio=`bc <<< echo "scale=2; $x \* 0.1"`
+	precio=`echo "scale=2; $x*0.1+50 | bc`
 	echo "El precio es de $precio euros"
 	fi
 fi
